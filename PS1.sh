60bf2da3aba990b47d464a23396f31f1d7c5bce3
Bar Angel <bar@lightbitslabs.com>
2018-10-28 10:21:53 +0200

    PS1.sh: stylish terminal prompt

---
+++ PS1.sh
@@ -0,0 +1,21 @@
+source ${HOME}/.customize/style.sh
+
+GIT_BRANCH="(\$(__git_ps1 '%s'))"
+
+LB=$(echo "(LB)" | paint $RED | paint $BOLD)
+GIT=$(echo $GIT_BRANCH | paint $BLUE | paint $BOLD)
+DIR=$(echo "\W" | paint $CYAN | paint $BOLD)
+STATION=$(echo "\u@\H" | paint $GREEN | paint $BOLD)
+END=$(echo "\$" | paint $YELLOW | paint $BOLD)
+
+STATION_NAME="$(cat /etc/hostname)"
+
+if [[ "${STATION_NAME}" = *"build"* ]]; then
+    STATION="📡"
+else
+    STATION="💻"
+fi
+
+PS1="$STATION $LB $GIT $DIR$END "
+
+# PS1="$(echo -E "$PS1" | paint $BOLD)"
