9ade5a9b097d65090aca4409be38aa9c8c8bd529
Bar Angel <bar@lightbitslabs.com>
2018-10-28 10:21:33 +0200

    style.sh: decorate your prints easily
    adding new bash commands that can be used to decorate prints.

---
+++ style.sh
@@ -0,0 +1,36 @@
+RED=91
+GREEN=92
+YELLOW=93
+BLUE=94
+MAGENTA=95
+CYAN=96
+WHITE=97
+BLACK=30
+COLOR_RESET=39
+
+BOLD=1
+UNDERLINE=4
+FORMAT_RESET=0
+
+
+FINISH="\]"
+START="\["
+
+_wrapup(){
+    echo -E "$START$1$FINISH"
+}
+
+reset_style(){
+    _wrapup "\e[${FORMAT_RESET}m"
+}
+
+bashstyle(){
+    _wrapup "\e[$1m"
+}
+
+paint(){
+    read -r TEXT
+    COLOR=$(_wrapup "\e[$1m")
+    RESET=$(_wrapup "\e[${FORMAT_RESET}m")
+    echo -E "$COLOR$TEXT$RESET"
+}
