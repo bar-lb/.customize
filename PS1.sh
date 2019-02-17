8e9dfe2b0c89665d47425bd89d30f430da8b0e92
bar-lb <bar@lightbitslabs.com>
2019-02-17 16:29:08 +0200

    removed lbprompt, show hostname instead

--- PS1.sh
+++ PS1.sh
@@ -2,12 +2,12 @@ source ${HOME}/.customize/style.sh
 
 GIT_BRANCH="(\$(__git_ps1 '%s'))"
 
-LB=$(echo "(\$(cat ${HOME}/.lbprompt))" | paint $RED | paint $BOLD)
+STATION_NAME="$(cat /etc/hostname)"
+
+HOST=$(echo $STATION_NAME | paint $RED | paint $BOLD)
 GIT=$(echo $GIT_BRANCH | paint $BLUE | paint $BOLD)
 DIR=$(echo "\W" | paint $CYAN | paint $BOLD)
 END=$(echo "\$" | paint $YELLOW | paint $BOLD)
-
-STATION_NAME="$(cat /etc/hostname)"
 
 if [[ "${STATION_NAME}" = *"build"* ]]; then
     STATION="📡"
@@ -15,6 +15,6 @@ else
     STATION="💻"
 fi
 
-PS1="$STATION $LB $GIT $DIR$END "
+PS1="$STATION $HOST $GIT $DIR$END "
 
 # PS1="$(echo -E "$PS1" | paint $BOLD)"
