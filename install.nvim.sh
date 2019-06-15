981d6622d548b95f59ee5fcd9b9e15e69ad937c8
Bar Angel <bar@lightbitslabs.com>
2019-06-15 10:16:23 +0300

    fix vim install

--- install.nvim.sh
+++ install.nvim.sh
@@ -5,4 +5,5 @@ cp $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.backup
 cp $HOME/.customize/nvim/init.vim $HOME/.config/nvim/init.vim
 
 cp -r $HOME/.customize/nvim/colors $HOME/.config/nvim/
+cp -r $HOME/.customize/nvim/after $HOME/.config/nvim/
 echo "nvim plugins & colors installed! open nvim and run InstallPlugins"
