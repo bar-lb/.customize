2e5f3f05caa273b3f7d59e4af6b17b93de857032
Bar Angel <bar@lightbitslabs.com>
2019-12-30 17:18:21 +0200

    change symbol and add workspace name to PS1

--- PS1.sh
+++ PS1.sh
@@ -1,5 +1,17 @@
 source ${HOME}/.customize/style.sh
 
+get_workspace_name() {
+    echo ${WORKSPACE_TOP##*/}
+}
+
+make_symbol() {
+    if [[ "$WORKSPACE_TOP" = *"universe"* ]]; then
+        echo "🛸"
+    else
+        echo "🥧"
+    fi
+}
+
 GIT_BRANCH="(\$(__git_ps1 '%s'))"
 
 STATION_NAME="$(cat /etc/hostname)"
@@ -7,14 +19,10 @@ STATION_NAME="$(cat /etc/hostname)"
 HOST=$(echo $STATION_NAME | paint $RED | paint $BOLD)
 GIT=$(echo $GIT_BRANCH | paint $BLUE | paint $BOLD)
 DIR=$(echo "\W" | paint $CYAN | paint $BOLD)
+WORKSPACE=$(echo "|\$(get_workspace_name)|" | paint $MAGENTA | paint $BOLD)
 END=$(echo "\$" | paint $YELLOW | paint $BOLD)
+SYMBOL="$(echo "\$(make_symbol)")"
 
-if [[ "${STATION_NAME}" = *"build"* ]]; then
-    STATION="📡"
-else
-    STATION="💻"
-fi
-
-PS1="$STATION $HOST $GIT $DIR$END "
+PS1="$SYMBOL $HOST $GIT $WORKSPACE $DIR$END "
 
 # PS1="$(echo -E "$PS1" | paint $BOLD)"
